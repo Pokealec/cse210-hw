9e70a07c4182153c458a4978cdaf33ac7390367f
Miles <mcd22005@byui.edu>
2023-09-19 19:38:28 -0600

    Did Prep3-5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Drawing;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+    int guess = -1;
+
+    Console.WriteLine("Hello Prep3 World!");
+    
+    Random randomGenerator = new Random();
+    int magicNumber = randomGenerator.Next(1, 11);
+
+while (guess != magicNumber){
+    Console.WriteLine("What is your guess?");
+    guess = int.Parse(Console.ReadLine());
+
+    if (magicNumber > guess){
+        Console.WriteLine("Higher");
+    }
+    else if (magicNumber < guess){
+        Console.WriteLine("Lower");
+    }
+    else {
+        Console.WriteLine("You got it!! :) ");
+    }
+
+    }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int input = -1;
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter your numbers.");
+        Console.WriteLine("Enter '0' when done.");
+
+    while (input != 0){
+        Console.Write("Enter Number: ");
+        string userInput = Console.ReadLine();
+        input = int.Parse(userInput);
+
+        if (input != 0){
+        numbers.Add(input);}
+    }
+    int sum = 0;
+    foreach (int number in numbers){
+            sum += number;
+        }
+    Console.WriteLine($"The sum of your numbers is: {sum}");
+
+    float average = ((float)sum) / numbers.Count;
+    Console.WriteLine($"The average is: {average}");
+
+    int max = numbers[0];
+        foreach (int number in numbers){
+            if (number > max){
+                max = number;
+        }}
+    Console.WriteLine($"The max is: {max}");
     }
 }
